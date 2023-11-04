99b9a05aa72e9a469ddc37229ca64f145303cd95
xpadev <xpadev@gmail.com>
2023-11-04 16:27:26 +0900

    fix: add cors

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ import {
   sessionMiddleware,
   CookieStore
 } from 'https://deno.land/x/hono_sessions/mod.ts'
+import {cors } from "https://deno.land/x/hono@v3.9.2/middleware.ts";
 import {setupBackend} from "./backend.ts";
 
 const app = new Hono<{
@@ -15,14 +16,23 @@ const app = new Hono<{
 }>()
 const store = new CookieStore()
 
-app.use('*', sessionMiddleware({
+app.use('/api/*', sessionMiddleware({
   store,
   encryptionKey: 'password_at_least_32_characters_long', // Required for CookieStore, recommended for others
-  expireAfterSeconds: 43200, // Expire session after 15 minutes
+  expireAfterSeconds: 43200,
   cookieOptions: {
     sameSite: 'Lax',
   },
 }))
+app.use(
+  '/api/*',
+  cors({
+    origin: '*',
+    allowMethods: ['POST', 'GET'],
+    maxAge: 43200,
+    credentials: true,
+  })
+)
 
 setupBackend(app);
 
