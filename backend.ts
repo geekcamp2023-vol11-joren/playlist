e052908331ece0c3fb7243bf6f284148d74f381e
XPA <xpadev@gmail.com>
2023-11-04 13:40:39 +0900

    feat: backend

--- backend.ts
+++ backend.ts
@@ -1,21 +1,60 @@
 import {Hono} from "https://deno.land/x/hono@v3.9.2/hono.ts";
+import {uuid} from "./utils/uuid.ts";
+import {UUID} from "./types/brand";
 
-const rooms:{[key:string]:(val:unknown)=>void[]} = {};
+const rooms:{[key:UUID]:{
+  playlist: string[],
+  handlers: (val:unknown)=>void[],
+  owner: UUID;
+}} = {};
+
+const broadcastPlaylistUpdate = (roomId: UUID) => {
+  const room = rooms[roomId];
+  if (room === undefined) return;
+  room.handlers.forEach((h) => h(room.playlist));
+}
 
 const setupBackend = (app: Hono) => {
-  app.get("/ws/:id", (c) => {
+  app.get("/room/:id/ws", (c) => {
     const roomId = c.req.param("id");
+    if (rooms[roomId] === undefined) {
+      return c.text("Room not found", 404);
+    }
     const { response, socket } = Deno.upgradeWebSocket(c.req);
     
     socket.addEventListener("message", (e) => console.log(e));
     const handler = (val) => socket.send(JSON.stringify(val));
-    rooms[roomId] ??= [];
-    rooms[roomId].push(handler);
+    rooms[roomId].handlers.push(handler);
     socket.onclose = () => {
-      rooms[roomId] = rooms[roomId].filter((s) => s !== handler);
+      rooms[roomId].handlers = rooms[roomId].handlers.filter((s) => s !== handler);
     }
     return response;
   });
+  app.post("/create",(c)=>{
+    const session = c.get('session');
+    const roomId = uuid();
+    session.id ??= uuid();
+    rooms[roomId] = {
+      handlers: [],
+      owner: session.id
+    };
+    return c.json({
+      roomId
+    });
+  });
+  app.post("/room/:id/add",async(c)=>{
+    const body = await c.req.json<{url: string}>()
+    const roomId = c.req.param("id");
+    const room = rooms[roomId];
+    const session = c.get('session');
+    session.id ??= uuid();
+    if (room === undefined) {
+      return c.text("Room not found", 404);
+    }
+    room.playlist.push(body.url);
+    broadcastPlaylistUpdate(roomId);
+    return c.text("OK");
+  })
 }
 
 export {setupBackend}
